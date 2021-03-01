671caf49040f2743c30d4c5fefb8dccf7d2cfdfc
mukhammadrasul <79836963+mukhammadrasul@users.noreply.github.com>
2021-03-01 15:31:42 +0500

    my first work
    my first work is done in github desktop.

---
+++ main.py
@@ -0,0 +1,73 @@
+#number = int(input("juft son kiriting."))
+#for i in range(0,100,2):
+ #   if number == i:
+  #      print("rahmat")
+#else:
+ #   print("juft son kiriting.")
+#age = int(input("yoshingiz nechida?"))
+#if age <=4 or age >=60:
+ #    price = 0
+#elif age < 18:
+ #    price = 18000
+#elif age > 18 :
+ #    price = 20000
+#else:
+ #    price = 22000
+#print(f"sizga kirish {price} som boladi")
+
+#f_number = int(input("birinchi sonni kiriting."))
+#s_number = int(input("ikkinchi sonni kiriting."))
+#if f_number == s_number:
+ #   print("siz kiritgan raqamlar bir biriga teng.")
+#elif f_number > s_number:
+ #   print(f"{f_number} > {s_number} ")
+#else:
+ #   print(f"{f_number} < {s_number} ")
+
+#mahsulotlar = ['bodring','piyoz','uzum','zira','tuz']
+#savat = []
+#print("kamida 5 ta mahsulot kiriting.")
+#bor_mahsulotlar = []
+#mavjud_emas = []
+#for i in range(3):
+#   savat.append(input(f"{i+1} - mahsulotni kiriting."))
+#for mahsulot in savat:
+#            if mahsulot in mahsulotlar:
+#                bor_mahsulotlar.append(mahsulot)
+#            else:
+#                mavjud_emas.append(mahsulot)
+#if mavjud_emas:
+#            print(f" bizda quyidagi mahsulotlar yoq. ")
+#            for mahsulot in mavjud_emas:
+#                print(mahsulot)
+#else:
+#            print("bizda siz soragan barcha mahsulotlar bor.")
+users = ['alisher1983','aziza','yasina' 'umar']
+
+login = input("Yangi login tanlang:")
+
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
+
+
+
+
+#users = ['umar','ahmad','anvar','jasur','hamid','akmal']
+#new = input("loginni kiriting. ")
+#if new in users:
+#        print("login band. yangi login tanlang.")
+#else:
+#        print(new , "xush kelibsiz!!!")
+
+
+
+#son = int(input("istalgan butun son kiriting."))
+#for i in range(2,11):
+ #   if  not son % i:
+  #      print( son , f" {i} soniga qoldiqsiz bolinadi.")
+
+
+
+
